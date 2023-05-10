f5f9cc4c77bf87ffed220350636aefb8ef3addab
Aleš Bernik <ales.bernik@gora.si>
2023-05-10 10:22:01 +0200

    document.baseURI used

--- projects/yti-common-ui/src/lib/init/keycloak-init.factory.ts
+++ projects/yti-common-ui/src/lib/init/keycloak-init.factory.ts
@@ -22,7 +22,7 @@ export function initializeKeycloak(
             initOptions: {
               onLoad: 'check-sso',
               checkLoginIframe: true,
-              silentCheckSsoRedirectUri: `${window.location.origin}/assets/silent-check-sso.html`,
+              silentCheckSsoRedirectUri: `${document.baseURI}/assets/silent-check-sso.html`,
               messageReceiveTimeout: 5000
             }
           })
